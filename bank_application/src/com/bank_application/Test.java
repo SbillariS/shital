106d6d5a2e83c6ff2a303160e6bf65a0df733193
Dell <Dell@DESKTOP-RA5RK60>
2024-05-16 15:58:08 +0530

    changes by shital 1

--- bank_application/src/com/bank_application/Test.java
+++ bank_application/src/com/bank_application/Test.java
@@ -2,6 +2,10 @@ package com.bank_application;
 
 public class Test 
 {
+	public void cheak_balance()
+	{
+		System.out.println("");
+	}
     public static void main(String args[])
   {
     System.out.println("This is main method");
