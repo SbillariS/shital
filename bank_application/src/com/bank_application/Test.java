bcbcd204c3a517b9d0a490a241fa29f003387b39
SbillariS <110086298+SbillariS@users.noreply.github.com>
2024-05-16 15:53:02 +0530

    Update Test.java
    changes by kiran

--- bank_application/src/com/bank_application/Test.java
+++ bank_application/src/com/bank_application/Test.java
@@ -1,5 +1,9 @@
 package com.bank_application;
 
-public class Test {
-
+public class Test 
+{
+    public static void main(String args[])
+  {
+    System.out.println("This is main method");
+  }
 }
